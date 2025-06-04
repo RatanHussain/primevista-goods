f8837d79887780d01a4986cd2975e9925cec8b24
Ratan Hussain <mdratanmia7395@gmail.com>
2025-06-04 14:57:40 +0300

    updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,22 +33,39 @@ function Home() {
 	return (
 		<div className='flex flex-col bg-gradient-to-r from-yellow-200 via-pink-200 to-pink-300 min-h-screen pt-28 md:pt-24 '>
 			<Navbar />
-			<h1 className='text-3xl md:text-4xl font-bold text-gray-800 mb-4 text-center mt-5 px-4'>
-				Start Your Amazon FBA Journey — Build a Business While You Study
+			<h1 className='text-2xl md:text-4xl font-bold text-gray-800 mb-4 text-center mt-5 px-4'>
+				Start Your Amazon FBA Journey
 			</h1>
 
-			<p className='text-base md:text-lg text-gray-700 text-center max-w-2xl mx-auto mb-6 px-4'>
+			{/* <p className='text-base md:text-lg text-gray-700 text-center max-w-2xl mx-auto mb-6 px-4'>
 				What if you could start earning online while still in school or college?
 				Our step-by-step Amazon FBA course is built for beginners — especially
 				students — who want to learn how to start a real business on Amazon and
 				earn passive income.
+			</p> */}
+			<p className='text-base md:text-lg text-gray-700 text-center max-w-2xl mx-auto mb-6 px-4'>
+				স্কুল বা কলেজে পড়াশুনা করার সময় যদি আপনি অনলাইনে আয় শুরু করতে পারেন,
+				তাহলে কেমন হবে? আমাদের ধাপে ধাপে Amazon FBA কোর্সটি নতুনদের জন্য তৈরি
+				করা হয়েছে - বিশেষ করে শিক্ষার্থীদের জন্য - যারা Amazon-এ একটি বাস্তব
+				ব্যবসা শুরু করতে এবং প্যাসিভ ইনকাম করতে শিখতে চান।
 			</p>
 
-			<p className='text-base md:text-lg text-gray-700 text-center max-w-2xl mx-auto mb-8 px-4'>
+			{/* <p className='text-base md:text-lg text-gray-700 text-center max-w-2xl mx-auto mb-8 px-4'>
 				🛒 Learn how to find winning products 📦 Launch your first store with
 				confidence 💸 Build a business that works while you sleep
+			</p> */}
+			<p className='text-base md:text-lg text-gray-700 text-center max-w-2xl mx-auto mb-8 px-4'>
+				🛒 বিজয়ী পণ্যগুলি কীভাবে খুঁজে পাবেন তা শিখুন 📦 আত্মবিশ্বাসের সাথে
+				আপনার প্রথম দোকানটি চালু করুন 💸 এমন একটি ব্যবসা তৈরি করুন যা আপনার
+				ঘুমের সময় কাজ করে
 			</p>
 
+			<h1 className='text-4xl md:text-4xl font-bold text-center'>
+				Your success is our goal.
+			</h1>
+			<h1 className='text-2xl md:text-4xl mt-5 font-bold text-center'>
+				আপনার সাফল্যই আমাদের লক্ষ্য
+			</h1>
 			<div className='max-w-sm mx-auto bg-white rounded-2xl shadow-lg p-6 border border-gray-200 text-center my-8'>
 				<h2 className='text-2xl font-bold text-gray-800 mb-2'>Exclusive</h2>
 				<h2 className='text-2xl md:text-3xl font-bold text-pink-600 mb-4'>
